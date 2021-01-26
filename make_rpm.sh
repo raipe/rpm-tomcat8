282160354d9ca82404551a60b8c666e4a43c0ae4
Raimo Järvi <raimo.jarvi@gmail.com>
2021-01-26 23:07:02 +0200

    Fix make_rpm.sh

--- make_rpm.sh
+++ make_rpm.sh
@@ -1,37 +1,37 @@
+#!/bin/bash
+
+set -e
+
 VERSION=$(grep "Version:" tomcat8.spec |cut -d ":" -f2 |tr -d "[:space:]")
 RELEASE=$(grep "Release:" tomcat8.spec |cut -d ":" -f2 |tr -d "[:space:]")
 ARCH=$(grep "BuildArch:" tomcat8.spec |cut -d ":" -f2 |tr -d "[:space:]")
 
 echo "Version: $VERSION-$RELEASE BuildArch: $ARCH"
 
-rm -rf rpmbuild
-mkdir rpmbuild
-mkdir rpmbuild/BUILD
-mkdir rpmbuild/RPMS
-mkdir rpmbuild/SOURCES
-mkdir rpmbuild/SPECS
-mkdir rpmbuild/SRPMS
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+RPMBUILD="${DIR}/build"
+
+rm -rf ${RPMBUILD}/BUILD
+rm -rf ${RPMBUILD}/SRPMS
+rm -rf ${RPMBUILD}/RPMS
+rm -f apache-tomcat-$VERSION.tar.gz apache-tomcat-$VERSION.tar.gz.old
 
 wget http://archive.apache.org/dist/tomcat/tomcat-8/v$VERSION/bin/apache-tomcat-$VERSION.tar.gz -O apache-tomcat-$VERSION.tar.gz
 
-##########################
-# Add custom tomfoolery
-# POC- Nick
-##########################
 tar -xzpf apache-tomcat-$VERSION.tar.gz
-cp marlin/marlin*.jar apache-tomcat-$VERSION/lib
-cp java-libs apache-tomcat-$VERSION/conf
+
 mv apache-tomcat-$VERSION.tar.gz apache-tomcat-$VERSION.tar.gz.old
 cd apache-tomcat-$VERSION/webapps/manager/
 jar -cvf manager.war *
 cd -
 tar -czpf apache-tomcat-$VERSION.tar.gz apache-tomcat-$VERSION
 
-ln -v -s "$(pwd)/apache-tomcat-$VERSION.tar.gz" "rpmbuild/SOURCES/"
-ln -v -s "$(pwd)/tomcat8."{init,logrotate,sysconfig,bin,conf} "rpmbuild/SOURCES/"
-ln -v -s "$(pwd)/tomcat8.spec" "rpmbuild/SPECS/"
+RPM_OPTS=("-bb"
+  "--define" "_topdir ${RPMBUILD}"
+  "--define" "_sourcedir ${DIR}"
+  "--define" "_buildrootdir %{_tmppath}"
+  "--target" "${ARCH}"
+)
 
-cd rpmbuild
-
-rpmbuild --buildroot "`pwd`/BUILDROOT" ../tomcat8.spec -bb --define "_topdir `pwd`"
-rpmbuild --buildroot "`pwd`/BUILDROOT" ../tomcat8-manager.spec -bb --define "_topdir `pwd`"
+rpmbuild tomcat8.spec "${RPM_OPTS[@]}"
+rpmbuild tomcat8-manager.spec "${RPM_OPTS[@]}"
